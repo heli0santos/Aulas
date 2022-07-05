ff8199b070ab486e9f0c56629f509f64aaa5e542
Hélio Santos <helio.santos@gmail.com>
2022-07-05 16:20:26 -0400

    udemy

--- exemplo8.py
+++ exemplo8.py
@@ -1,12 +1,12 @@
 print('\t ----Cálculo do novo salário---- ')
 salario_atual = float(input('Informe o salário atual: '))
 
-if (salario_atual<500):
+if salario_atual < 500:
     salario_novo = salario_atual+(salario_atual*0.15)
     print('Salário com reajuste = ', salario_novo)
-if ((salario_atual >= 500) and (salario_atual <= 1000)):
+elif salario_atual >= 500 and salario_atual <= 1000:
     salario_novo = salario_atual+(salario_atual*0.10)
     print('Salário com reajuste = ', salario_novo)
-if (salario_atual>1000):
+else:
     salario_novo = salario_atual+(salario_atual*0.05)
     print('Salário com reajuste = ', salario_novo)
